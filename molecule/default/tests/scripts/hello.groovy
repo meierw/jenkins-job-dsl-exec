0eb509e55ce10a8a966bfd219dec0c6befc4ef12
Valters Meirēns <valters.meirens@tieto.com>
2018-12-14 09:11:45 +0200

    Experimenting...

---
+++ molecule/default/tests/scripts/hello.groovy
@@ -0,0 +1,4 @@
+String bar = "foo"
+String hello = "hello world"
+String foo = "bar"
+println hello
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,21 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_jenkins(host):
+    output = host.check_output(get_script_curl_command('hello.groovy'))
+    assert output == 'hello world'
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    output = host.check_output(get_script_curl_command('version.groovy'))
+    assert output == '2.4.12'
+
+
+def get_script_curl_command(script_file):
+    script_dir = os.path.dirname(__file__) + '/scripts/'
+    script_content = open(script_dir + script_file, 'r').read()
+
+    # script passed in 'single quotes', so scripts themselves should avoid using them
+    return (
+        'curl -u admin:admin ' +
+        "--data-urlencode 'script=" + script_content + "' " +
+        'http://localhost:8080/scriptText'
+    )
